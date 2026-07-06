6760a70e44920626740d3cee19272953a7e23c38
sulthanhafizh2006 <sulthanhafizh2006@student.uns.ac.id>
2026-07-06 22:49:44 +0700

    Update order.py

--- nia_heap_integrasi/order.py
+++ nia_heap_integrasi/order.py
@@ -1,17 +1,17 @@
 from datetime import datetime
 
-# level prioritas untuk digunakan di heap nanti, semakin kecil semakin diprioritaskan
-
+# level prioritas buat dipake di heap nanti, makin kecil makin diutamain
 PRIORITAS_LAYANAN = {
-    "Express": 1,
-    "Reguler": 2,
+    "express": 1,
+    "reguler": 2,
     "cuci_kering": 3,
 }
 
+
 class Order:
-    def __init__(self,order_id, nama_pelanggan, jenis_layanan, berat_kg):
+    def __init__(self, order_id, nama_pelanggan, jenis_layanan, berat_kg):
         self.order_id = order_id
-        self.nama_pelanggan =  nama_pelanggan
+        self.nama_pelanggan = nama_pelanggan
         self.jenis_layanan = jenis_layanan.lower().strip()
         self.berat_kg = berat_kg
         self.prioritas = PRIORITAS_LAYANAN.get(self.jenis_layanan, 2)
@@ -19,13 +19,13 @@ class Order:
         self.waktu_masuk = datetime.now().strftime("%H:%M:%S")
 
     def estimasi_biaya(self):
-        harga_per_kg = {"Express": 10000, "Reguler": 6000, "cuci_kering": 7000}
+        harga_per_kg = {"express": 10000, "reguler": 6000, "cuci_kering": 7000}
         tarif = harga_per_kg.get(self.jenis_layanan, 6000)
         total = tarif * self.berat_kg
-        if self.jenis_layanan == "Express":
+        if self.jenis_layanan == "express":
             total += 5000
         return total
-    
+
     def __str__(self):
-        return (f"[#{self.order_id}]{self.nama_pelanggan} - {self.jenis_layanan_upper()}"
+        return (f"[#{self.order_id}] {self.nama_pelanggan} - {self.jenis_layanan.upper()} "
                 f"({self.berat_kg}kg) | prioritas={self.prioritas} | {self.status} | jam {self.waktu_masuk}")
